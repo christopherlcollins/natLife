c7ac3d77d94d9c3d85920db429490b062745976e
christopherlcollins <christopher.l.collins@gmail.com>
2015-03-29 01:30:31 -0500

    Structural integrity

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-//components
+//-----components
+
 
 var nextButton;
 var prevButton;
 var upButton;
 var downButton;
-var title;
-var subtitle;
+var nextTitle;
+var prevTitle;
+var activeTitle;
+var nextSubtitle;
+var prevSubtitle;
+var activeSubtitle;
 var activeVid;
 var nextVid;
 var prevVid;
 var pauseMenu;
+var characterSection;
+var subCharacterSection;
 
-//states
+
+//-----states
+var isViewingMainCharacters;
+var isViewingSubCharacters;
 var isHoveringOnNext;
 var isTransitioningToNext;
 var isHoveringOnPrev;
@@ -24,34 +34,116 @@ var isPlaying;
 var isPaused;
 
 
-//interactions
+//-----------------interactions-----------------
 
-function preNext(){}
+function startNext(){}
+function cancelNext(){}
 
-function next(){}
 
-function prePrev(){}
+function next(){
+	
+//active becomes previous
+//previous is removed
+//next becomes active
+//add next
 
-function prev(){}
+activeVid.removeClass('active');
+activeVid.addClass('previous');
+prevVid.remove();
+nextVid.removeClass('next');
+nextVid.addClass('active');
+appendElement();	
+	
+	
+}
 
-function preUp(){}
+function startPrev(){}
+function cancelPrev(){}
 
-function up(){}
+function prev(){
+	
+//active becomes next
+//next is removed
+//previous becomes active
+//add previous
 
-function preDown(){}
+activeVid.removeClass('active');
+activeVid.addClass('next');
+nextVid.remove();
+prevVid.removeClass('previous');
+prevVid.addClass('active');
+prependElement();	
 
-function down(){}
+	
+	
+}
+
+function startUp(){}
+function cancelUp(){}
+
+
+function up(){
+	
+//characters becomes below
+//sub characters becomes active	
+
+characterSection.removeClass('sectionActive');	
+characterSection.addClass('sectionBelow');
+subCharacterSection.removeClass('sectionAbove');
+subCharacterSection.addClass('sectionActive');	
+	
+}
+
+function startDown(){}
+function cancelDown(){}
+
+function down(){
+
+//subCharacters becomes above
+//characters becomes active
+	
+characterSection.removeClass('sectionActive');	
+characterSection.addClass('sectionBelow');
+subCharacterSection.removeClass('sectionAbove');
+subCharacterSection.addClass('sectionActive');	
+	
+	
+}
 
 function playPause(){}
 
 function chapterNext(){}
 
 
-//major actions
+//-----------------major actions-----------------
 
-function bindComponents(){}
+function bindComponents(){
+
+var nextButton = $('#buttonNext');
+var prevButton = $('#buttonNext');
+var upButton = $('#buttonNext');
+var downButton = $('#buttonNext');
+var nextTitle = $('.next').find('.title');
+var prevTitle = $('.previous').find('.title');
+var activeTitle = $('.active').find('.title');
+var nextSubtitle = $('.next').find('.subtitle');
+var prevSubtitle = $('.previous').find('.subtitle');
+var activeSubtitle = $('.active').find('.subtitle');
+var activeVid = $('.active').find('video');
+var nextVid = $('.next').find('video');
+var prevVid = $('.previous').find('video');
+var pauseMenu = $('.pauseMenu');
+var characterSection = $('#characters');
+var subCharacterSection = $('subCharacters');
+	
+}
 
 
-//sub actions
+function appendElement(){}
+function prependElement(){}
+
+
+//-----------------sub actions-----------------
 
 function animateUI(){}
+
