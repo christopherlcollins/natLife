2f319aa4ed4969878d7575f03bb411478de4f471
christopherlcollins <christopher.l.collins@gmail.com>
2015-03-28 23:29:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+//components
+
+var nextButton;
+var prevButton;
+var upButton;
+var downButton;
+var title;
+var subtitle;
+var activeVid;
+var nextVid;
+var prevVid;
+
+//states
+var isHoveringOnNext;
+var isTransitioningToNext;
+var isHoveringOnPrev;
+var isTransitioningToPrev;
+var isHoveringOnUp;
+var isTransitioningToUp;
+var isHoveringOnDown;
+var isTransitioningToDown;
+var isPlaying;
+var isPaused;
+
+
+//interactions
+
+function preNext(){}
+
+function next(){}
+
+function prePrev(){}
+
+function prev(){}
+
+function preUp(){}
+
+function up(){}
+
+function preDown(){}
+
+function down(){}
+
+function playPause(){}
+
+function chapterNext(){}
+
+
+//major actions
+
+
+//sub actions
+
